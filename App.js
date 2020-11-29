76328b0b119e3c2dac83a3e8ec067110c88d8ebc
Samk13 <samarbid13@gmail.com>
2020-11-29 19:28:17 +0100

    playng around with saveareaview and touchables

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  Image,
+  View,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
+  let clickNumber = 0;
+  const clicked = () => {
+    console.log(`the text has been clicked ${clickNumber++}`);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={clicked}>
+        Hello from Sam 👋
+      </Text>
+      <TouchableNativeFeedback onPress={clicked}>
+        <View style={styles.view}></View>
+      </TouchableNativeFeedback>
+      <Image
+        fadeDuration={2000}
+        source={{
+          uri: "https://picsum.photos/id/2/200/300",
+          width: 200,
+          height: 300,
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  view: {
+    width: 200,
+    height: 70,
+    backgroundColor: "dodgerblue",
   },
 });
